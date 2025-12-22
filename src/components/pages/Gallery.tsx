cdc7b4b697450d9f792d15405994591a2a727c09
KingJamesGOAT <sbstevebenjamin@gmail.com>
2025-12-22 19:26:32 +0100

    style: adjust gallery grid to 3 columns on mobile and 5 on desktop

--- src/components/pages/Gallery.tsx
+++ src/components/pages/Gallery.tsx
@@ -28,7 +28,7 @@ export default function Gallery() {
           </div>
         </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-3 lg:grid-cols-5 gap-3 sm:gap-6">
           {[...Array(6)].map((_, i) => (
             <motion.div
               key={i}
